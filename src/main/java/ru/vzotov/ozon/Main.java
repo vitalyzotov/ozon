adfa4be6f52eda282bad5aaaa04337040c1c7038
vzotov <vitalyzotov@gmail.com>
2023-05-17 09:26:12 +0400

    Keep code clean

--- src/main/java/ru/vzotov/ozon/Main.java
+++ src/main/java/ru/vzotov/ozon/Main.java
@@ -1,16 +1,11 @@
 package ru.vzotov.ozon;
 
-import io.netty.handler.logging.LogLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.transport.logging.AdvancedByteBufFormat;
 import ru.vzotov.ozon.model.ClientOperations;
 import ru.vzotov.ozon.model.ClientOperationsFilter;
 import ru.vzotov.ozon.model.ClientOperationsRequest;
@@ -25,9 +20,6 @@ import java.time.YearMonth;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Optional;
-
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
@@ -72,42 +64,4 @@ public class Main implements CommandLineRunner {
                 });
     }
 
-    private void play() {
-        HttpClient httpClient = HttpClient
-                .create()
-                .wiretap("reactor.netty.http.client.HttpClient",
-                        LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL);
-
-        WebClient webClient = WebClient.builder()
-                .codecs(codecs -> {
-                    codecs.defaultCodecs()
-                            .configureDefaultCodec(conf -> {
-                                log.info("codec {}", conf);
-                            });
-                })
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .baseUrl("https://cat-fact.herokuapp.com")
-                .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-                .build();
-
-        Mono<String> error = webClient.post().uri(b -> b.pathSegment("facts", "random").build())
-                .bodyValue(new ClientOperationsRequest(
-                        new Cursors(),
-                        new ClientOperationsFilter(
-                                Collections.emptyList(),
-                                new DateRange(YearMonth.now()),
-                                ClientOperationsFilter.EFFECT_UNKNOWN
-                        ),
-                        1,
-                        30
-                ))
-                .retrieve().bodyToMono(String.class);
-        error.block();
-
-        Mono<String> response = webClient.get().uri(b -> b.pathSegment("facts", "random").build())
-                .retrieve()
-                .bodyToMono(String.class);
-        String fact = response.block();
-        log.info("Got {}", fact);
-    }
 }
